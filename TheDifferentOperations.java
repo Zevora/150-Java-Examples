b0c8c6712ae9110a07a10050a7d4a73ebbfcaa1c
Lyan Peña <34713514+Zevora@users.noreply.github.com>
2019-10-18 14:23:01 -0400

    TheDifferentOperations
    Adding TheDifferentOperations java file.
    
    Awaiting Verification

---
+++ TheDifferentOperations.java
@@ -0,0 +1,11 @@
+public class TheDifferentOperations
+{
+    public static void main(String[] args)
+    {
+        int operationA = -5 + 8 * 6;
+        double operationB = (55+9) % 9;
+        double operationC = 20 + -3*5 / 8;
+        double operationD = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println("\nOperations: \nOperationA: " + operationA + "\n" + "OperationB: " + operationB + "\n" + "OperationC: " + operationC + "\n" + "OperationD: " + operationD);
+    }
+}
